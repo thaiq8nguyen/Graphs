d2d30e5ba98ec8958a7618ac63686b6eed91227f
Thai Nguyen <discoverylight@yahoo.com>
2020-02-17 19:26:14 -0800

    updated graph answers

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -3,9 +3,11 @@ Simple graph implementation
 """
 from util import Stack, Queue  # These may come in handy
 
+
 class Graph:
 
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
+
     def __init__(self):
         self.vertices = {}
 
@@ -35,23 +37,23 @@ class Graph:
         Print each vertex in breadth-first order
         beginning from starting_vertex.
         """
-        #1. Create an empty queue
-        q = Queue()
-        #2. Add the starting vertex_id to the queue
+        # 1. Create an empty queue
+        q = Queue()  # FIFO
+        # 2. Add the starting vertex_id to the queue
         q.enqueue(starting_vertex)
-        #3. Create an empty set to store visited nodes
+        # 3. Create an empty set to store visited nodes
         visited = set()
-        #4. While the queue is not empty
+        # 4. While the queue is not empty
         while q.size() > 0:
-        #5. Dequeue, the first vertex
+            # 5. Dequeue, the first vertex
             vertex = q.dequeue()
-        #6. Check if it's been visited
+        # 6. Check if it's been visited
             if vertex not in visited:
-        #7. If is has not been visited
-            #8. Mark it as visited
+                # 7. If is has not been visited
+                # 8. Mark it as visited
                 print(vertex)
                 visited.add(vertex)
-            #9. Then add all neighbors to to the back of the Queue
+            # 9. Then add all neighbors to to the back of the Queue
                 for neighbor in self.get_neighbors(vertex):
                     q.enqueue(neighbor)
         print("==END BFT==")
@@ -62,26 +64,25 @@ class Graph:
         beginning from starting_vertex.
         """
         # The algorithm is the same as in BFT but we are using a Stack
-        #1. Create an empty stack
-        s = Stack()
-        #2. Add the starting vertex_id to the stack
+        # 1. Create an empty stack
+        s = Stack()  # LIFO
+        # 2. Add the starting vertex_id to the stack
         s.push(starting_vertex)
-        #3. Create an empty set to store the visited nodes
+        # 3. Create an empty set to store the visited nodes
         visited = set()
-        #4. While the stack is not empty
+        # 4. While the stack is not empty
         while s.size() > 0:
-            #5. Pop the first vertex
+            # 5. Pop the first vertex
             vertex = s.pop()
-            #6. Check if it's been visited
+            # 6. Check if it's been visited
             if vertex not in visited:
-                
                 visited.add(vertex)
                 print(vertex)
                 for neighbor in self.get_neighbors(vertex):
                     s.push(neighbor)
 
-
         print("==END DFT==")
+
     def dft_recursive(self, starting_vertex):
         """
         Print each vertex in depth-first order
@@ -90,6 +91,7 @@ class Graph:
         This should be done using recursion.
         """
         visited = set()
+
         def dft_recur(vertex):
             if vertex in visited:
                 return
@@ -97,7 +99,7 @@ class Graph:
             print(vertex)
             for v in self.vertices[vertex]:
                 dft_recur(v)
-        
+
         dft_recur(starting_vertex)
 
         print("==END DFT USING RECURSIVE==")
@@ -108,38 +110,37 @@ class Graph:
         starting_vertex to destination_vertex in
         breath-first order.
         """
-        #1. Create an empty queue and push a path to the starting vertex_id
+        # 1. Create an empty queue and push a path to the starting vertex_id
         q = Queue()
         q.enqueue([starting_vertex])
-        #2. Create a set to store visited vertices
+        # 2. Create a set to store visited vertices
         visited = set()
-        #3. While the queue is not empty
+        # 3. While the queue is not empty
         while q.size() > 0:
-            #4. dequeue the first path
+            # 4. dequeue the first path
             last_path = q.dequeue()
-            #5. Grab the last vertex from the path
+            # 5. Grab the last vertex from the path
             last_vertex = last_path[-1]
-            #6. If that vertex has not been visited:
+            # 6. If that vertex has not been visited:
             if last_vertex not in visited:
-            #7. Check if it's the destination
+                # 7. Check if it's the destination
                 if last_vertex == destination_vertex:
-                    
-            #8. If so:
-                    #9. Return path
+
+                    # 8. If so:
+                    # 9. Return path
                     return last_path
-                #9. Mark is as visited
+                # 9. Mark is as visited
                 visited.add(last_vertex)
-                #10. Add a path to its neighbors to the back of the queue
+                # 10. Add a path to its neighbors to the back of the queue
                 for v in self.vertices[last_vertex]:
 
-                #11. Copy the path
+                    # 11. Copy the path
                     new_path = [*last_path]
 
-                #12. Append the neighbor to the back of the queue
+                # 12. Append the neighbor to the back of the queue
                     new_path.append(v)
                     q.enqueue(new_path)
         print("==END BFS==")
-
 
     def dfs(self, starting_vertex, destination_vertex):
         """
@@ -147,34 +148,34 @@ class Graph:
         starting_vertex to destination_vertex in
         depth-first order.
         """
-        #1. Create an empty stack and push a path to the starting vertex_id
+        # 1. Create an empty stack and push a path to the starting vertex_id
         s = Stack()
         s.push([starting_vertex])
-        #2. Create a set to store visited vertices
+        # 2. Create a set to store visited vertices
         visited = set()
-        #3. While the stack is not empty
+        # 3. While the stack is not empty
         while s.size() > 0:
-            #4. dequeue the first path
+            # 4. dequeue the first path
             last_path = s.pop()
-            #5. Grab the last vertex from the path
+            # 5. Grab the last vertex from the path
             last_vertex = last_path[-1]
-            #6. If that vertex has not been visited:
+            # 6. If that vertex has not been visited:
             if last_vertex not in visited:
-            #7. Check if it's the destination
+                # 7. Check if it's the destination
                 if last_vertex == destination_vertex:
-                    
-            #8. If so:
-                    #9. Return path
+
+                    # 8. If so:
+                    # 9. Return path
                     return last_path
-                #9. Mark is as visited
+                # 9. Mark is as visited
                 visited.add(last_vertex)
-                #10. Add a path to its neighbors to the back of the queue
+                # 10. Add a path to its neighbors to the back of the queue
                 for v in self.vertices[last_vertex]:
 
-                #11. Copy the path
+                    # 11. Copy the path
                     new_path = [*last_path]
 
-                #12. Append the neighbor to the back of the queue
+                # 12. Append the neighbor to the back of the queue
                     new_path.append(v)
                     s.push(new_path)
         print("==END DFS==")
@@ -207,6 +208,7 @@ class Graph:
         ret_path.reverse()
 
         return ret_path
+
 
 if __name__ == '__main__':
     graph = Graph()  # Instantiate your graph
