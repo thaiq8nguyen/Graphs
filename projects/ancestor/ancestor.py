582248b7902320b127bf647277a24831574e7bc7
Thai Nguyen <discoverylight@yahoo.com>
2020-02-19 08:18:52 -0800

    completed ancestor exercise

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,34 @@
 
+import os
+import sys
+sys.path.append("../graph")
+from graph import Graph
+
+
+
+
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    g = Graph()
+
+    for v in range(1, 12):
+        g.add_vertex(v)
+    for a in ancestors:
+        g.add_edge(a[0], a[1])
+
+    paths = []
+
+    for a in g.vertices:
+        if g.dfs_recursive(a, starting_node) is not None and\
+                len(g.dfs_recursive(a, starting_node)) > 0:
+            paths.append(g.dfs_recursive(a, starting_node))
+
+    if len(paths) == 1:
+        return -1
+
+    ret_path = paths[0]
+    for p in paths:
+        if len(p) > len(ret_path) or\
+                (len(p) == len(ret_path) and p[0] < ret_path[0]):
+            ret_path = p
+
+    return ret_path[0]
