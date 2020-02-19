7a2f3a0f1f2d6530c6e17ee0683cabcf9dbe2599
Thai Nguyen <discoverylight@yahoo.com>
2020-02-19 13:43:27 -0800

    completed social network assignment

--- projects/social/social.py
+++ projects/social/social.py
@@ -1,6 +1,23 @@
+import random
+
+class Queue():
+    def __init__(self):
+        self.queue = []
+    def enqueue(self, value):
+        self.queue.append(value)
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+    def size(self):
+        return len(self.queue)
+
 class User:
     def __init__(self, name):
         self.name = name
+    def __repr__(self):
+        return self.name
 
 class SocialGraph:
     def __init__(self):
@@ -45,8 +62,28 @@ class SocialGraph:
         # !!!! IMPLEMENT ME
 
         # Add users
-
+        for i in range(num_users):
+            self.add_user(f"User {i + 1}")
         # Create friendships
+        possible_friendships = []
+        for user_id in self.users:
+            for friend_id in range(user_id + 1, self.last_id + 1):
+                possible_friendships.append((user_id, friend_id))
+        
+        random.shuffle(possible_friendships)
+        print("----")
+        print(possible_friendships)
+        print("----")
+
+        # Grab the first N parts from the list and create those friendships
+        for i in range(num_users * avg_friendships // 2):
+            friendship = possible_friendships[i]
+            self.add_friendship(friendship[0], friendship[1])
+        
+        # avg_friendships = total_friendships / num_users
+        # total_friendships = avg_friendships * num_users
+        # N = avg_friendships * num_users // 2
+
 
     def get_all_social_paths(self, user_id):
         """
@@ -59,6 +96,44 @@ class SocialGraph:
         """
         visited = {}  # Note that this is a dictionary, not a set
         # !!!! IMPLEMENT ME
+
+        def bfs(starting_vertex, destination_vertex):
+        
+            # 1. Create an empty queue and push a path to the starting vertex_id
+            q = Queue()
+            q.enqueue([starting_vertex])
+            # 2. Create a set to store visited vertices
+            visited = set()
+            # 3. While the queue is not empty
+            while q.size() > 0:
+                # 4. dequeue the first path
+                last_path = q.dequeue()
+                # 5. Grab the last vertex from the path
+                last_vertex = last_path[-1]
+                # 6. If that vertex has not been visited:
+                if last_vertex not in visited:
+                    # 7. Check if it's the destination
+                    if last_vertex == destination_vertex:
+
+                        # 8. If so:
+                        # 9. Return path
+                        return last_path
+                    # 9. Mark is as visited
+                    visited.add(last_vertex)
+                    # 10. Add a path to its neighbors to the back of the queue
+                    for v in self.friendships[last_vertex]:
+
+                        # 11. Copy the path
+                        new_path = [*last_path]
+
+                    # 12. Append the neighbor to the back of the queue
+                        new_path.append(v)
+                        q.enqueue(new_path)
+        
+        for u in self.users:
+            visited[u] = bfs(user_id,u)
+
+        # Breath first search guarantees a shortest path
         return visited
 
 
@@ -66,5 +141,6 @@ if __name__ == '__main__':
     sg = SocialGraph()
     sg.populate_graph(10, 2)
     print(sg.friendships)
+    # print(sg.users)
     connections = sg.get_all_social_paths(1)
     print(connections)
